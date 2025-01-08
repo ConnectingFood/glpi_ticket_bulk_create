d578fa8cabbe3e089548739881fb451300255351
unknown <josemartins.camargo@gmail.com>
2025-01-08 19:02:28 -0300

    mudanca data teste

--- src/domain/orgs/repositories/org_repository.py
+++ src/domain/orgs/repositories/org_repository.py
@@ -116,8 +116,8 @@ class OrgRepository(BaseRepository):
             )
             and emp.cliente_id=1
             and emp.cd_status=1
-            and ent.data_vencimento>=GETDATE()-15
-            and ent.data_vencimento<=GETDATE()+60
+            and ent.data_vencimento>=GETDATE()
+            and ent.data_vencimento<=GETDATE()+45
         order by ent.data_vencimento
         """
         self.execute(query=query)
